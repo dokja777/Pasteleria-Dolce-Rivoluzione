6bbf1a4f83d2a994ab62b542c988b8989ebf1df8
Alexandra-VP <146406971+Alexandra-VP@users.noreply.github.com>
2023-10-05 12:08:58 -0500

    US017

--- productos.php
+++ productos.php
@@ -60,32 +60,65 @@
   </script>
 
   <!--Filtrado-->
+  <main class="cuerpo">
+        <div class="header">
+           <div class="header-home"><a href="index.html"><i class="fa-solid fa-house"></i></a></div>
+        </div>
+        <hr>
+        <h1 class="title">Catálogo de Productos</h1>
+        <div class="contenido">
+         <div class="container">
+            <!--filtrado de productos-->
+             <aside class="aside">
+               <div class="titulo"><p>CATEGORÍAS</p></div>
+               <a href="#"><p>Tortas</p></a>
+               <a href="#"><p>Personalizado</p></a>
+               <a href="#"><p>Galletas</p></a>
+               <a href="#"><p>Cupcakes</p></a>
+               <a href="#"><p>Postres</p></a>
+             </aside>
 
+             <aside class="aside_filtro">
+              <div class="titulo_filtro"><p>FILTRO</p></div>
+              <p>Ordenar por:</p>
+              <select class="PRECIO" name="PRECIO" id="PRECIO">
+                <option value="Todos">Todos</option>
+                  <option value="DESC">De mayor a menor</option>
+                  <option value="ASC">De menor a mayor</option>
+              </select>
+              <br>
+                <button class="btn-buscar" type="submit" onclick="buscar_precio($('#PRECIO'.val()));">Buscar</button>
+             </aside>
+         </div>
+          <!--Productos-->
+          <!-- Catálogo de Productos -->
+          <div class="container__productos">
+            <?php
+              include ('config/conexion.php');
+              $query = "SELECT * FROM producto";
+              $resultado = $conexion->query($query);
+              while ($row = $resultado->fetch_assoc()) {
+              ?>
+            <div class="card">
+              <img src="data:image/jpg;base64, <?php echo base64_encode($row['IMG']); ?>">
+              <h4>
+                <?php echo $row['N_PRODUCTO']; ?>
+              </h4>
+              <p>
+                <a>S/</a>
+                <?php echo $row['PRECIO']; ?>
+              </p>
+              <button class="ver-detalle">Ver Detalle del Producto</button>
+            </div>
+            <?php
+            }
+          ?>
+          </div>
+           
+        </div> 
+  </main>
   
-  <!-- Catálogo de Productos -->
-  <h1 class="title">Catálogo de Productos</h1>
-  <div class="container__productos">
-    <?php
-      include ('config/conexion.php');
-      $query = "SELECT * FROM producto";
-      $resultado = $conexion->query($query);
-      while ($row = $resultado->fetch_assoc()) {
-      ?>
-    <div class="card">
-      <img src="data:image/jpg;base64, <?php echo base64_encode($row['IMG']); ?>">
-      <h4>
-        <?php echo $row['N_PRODUCTO']; ?>
-      </h4>
-      <p>
-        <a>S/</a>
-        <?php echo $row['PRECIO']; ?>
-      </p>
-      <button class="ver-detalle">Ver Detalle del Producto</button>
-    </div>
-    <?php
-    }
-  ?>
-  </div>
+
   <style>
     .title {
       text-align: center;
@@ -96,12 +129,16 @@
 
     .container__productos {
       width: auto;
+      column-count: 4;
       max-width: auto;
       height: auto;
-      display: flex;
-      flex-wrap: wrap;
       justify-content: center;
       overflow: hidden;
+      flex-basis: 0;
+      flex-grow: 999;
+      min-width: 60%;
+      margin-bottom: 30px;
+      display: wrap;
     }
 
     .container__productos .card {
@@ -183,6 +220,91 @@
 
     .container__productos .card:hover .ver-detalle {
       opacity: 1;
+    }
+    /*filtro*/
+    .cuerpo{
+      background: #f4f1f1;
+    }
+    .header{
+      display: flex;
+      padding-top: 10px;
+    }
+    .header .header-home{
+      padding: 10px 20px;
+    }
+    .header .header-home a{
+      color: #783f04;
+    }
+    p{
+      padding: 10px;
+    }
+    .header .header-txt{
+      padding: 10px 20px;
+      color: #783f04;
+    }
+    .container{
+    margin-top: 7px;
+    }
+    .contenedor-items{
+    margin: 20px 15px;
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    grid-gap: 30px;
+    }
+    .aside{
+    margin-top: 35px;
+    margin-left: 2%;
+    flex-basis: 17%;
+    background: #f9cb9c;
+    border-radius: 5px;
+    border: 1.5px solid #783f04;
+    }
+    .aside_filtro{
+    margin-top: 35px;
+    margin-left: 2%;
+    flex-basis: 17%;
+    background: #f9cb9c;
+    border-radius: 5px;
+    border: 1.5px solid #783f04;
+    }
+    .titulo p {
+    font-weight: bold;
+    text-align: center
+    }
+    .titulo_filtro p{
+    font-weight: bold;
+    text-align: center;
+    }
+    .aside_filtro p{
+    text-align: center;
+    }
+    .aside a{
+    color: #000;
+    text-align: center;
+    }
+    
+    .container{
+      flex-basis: 13rem;
+      flex-grow: 1;
+      height: 200px;
+
+    }
+    .PRECIO{
+    position: center;
+    margin-top: 5px;
+    margin-bottom: 10px;
+    margin-left: 30px;
+    }
+    .contenido{
+      display: flex;
+      flex-wrap: wrap;
+      gap: 1rem;
+    }
+
+    .btn-buscar{
+    margin-top: 5px;
+    margin-bottom: 10px;
+    margin-left: 70px;
     }
   </style>
 
