b624f839a1c2d706d4a2e90707ba491b501a65b6
Alexandra-VP <146406971+Alexandra-VP@users.noreply.github.com>
2023-10-09 13:22:40 -0500

    Update productos.php

--- productos.php
+++ productos.php
@@ -15,6 +15,11 @@ $sql_categorias = $conexion->query("SELECT * FROM categoria_producto") or die($c
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="css/style.css" />
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="http://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
   
   <!-------------------- Iconos en font awesome ------------------->
   <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
@@ -118,18 +123,56 @@ $sql_categorias = $conexion->query("SELECT * FROM categoria_producto") or die($c
 
 
 <!-------------------- Filtro mayor  a menor  ---------------------------------->
-        <aside class="aside_filtro">
-          <div class="titulo_filtro">
-            <p>FILTRO</p>
+      <aside class="aside_filtro">
+          <div class="col-md-3">                                
+              <div class="titulo_filtro">
+                  <p>PRECIO</p>
+                  <input type="hidden" id="hidden_minimum_price" value="0" />
+                  <input type="hidden" id="hidden_maximum_price" value="65000" />
+                  <p id="price_show"> 5 - 200</p>
+                  <div id="price_range" style="width:150px; margin:auto"></div>
+              </div>                
           </div>
-          <p>Ordenar por:</p>
-          <select class="PRECIO" name="PRECIO" id="PRECIO">
-            <option value="Todos">Todos</option>
-            <option value="DESC">De mayor a menor</option>
-            <option value="ASC">De menor a mayor</option>
-          </select>
-          <br>
-          <button class="btn-buscar" type="submit" onclick="buscar_precio($('#PRECIO'.val()));">Buscar</button>
+          <div class="col-md-9">
+              <br>
+            <div class="row filter_data">
+          </div>
+        </div>
+          
+            <script>
+            $(document).ready(function(){
+                filter_data();
+                function filter_data()
+                {
+                    $('.filter_data').html('<div id="loading" style="" ></div>');
+                    var action = 'fetch_data';
+                    var minimum_price = $('#hidden_minimum_price').val();
+                    var maximum_price = $('#hidden_maximum_price').val();
+                    $.ajax({
+                        url:"fetch_data.php",
+                        method:"POST",
+                        data:{action:action, minimum_price:minimum_price, maximum_price:maximum_price},
+                        success:function(data){
+                            $('.filter_data').html(data);
+                        }
+                    });
+                }
+                $('#price_range').slider({
+                    range:true,
+                    min:50,
+                    max:500,
+                    values:[50, 500],
+                    step:50,
+                    stop:function(event, ui)
+                    {
+                        $('#price_show').html(ui.values[0] + ' - ' + ui.values[1]);
+                        $('#hidden_minimum_price').val(ui.values[0]);
+                        $('#hidden_maximum_price').val(ui.values[1]);
+                        filter_data();
+                    }
+                });
+            });
+            </script>
         </aside>
       </div>
 <!----------------------------------------------------------------------------->
@@ -241,7 +284,7 @@ ul.categoria-lista li a:hover {
       text-align: center;
       font-size: 40px;
       color: #783f04;
-      margin-top: 100px;
+      margin-top: 30px;
     }
 
     .container__productos {
