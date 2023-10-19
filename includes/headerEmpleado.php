ed637eeab067cfa03cb3e117ca1570cfbb28688d
maria <128330171+dokja777@users.noreply.github.com>
2023-10-18 22:04:21 -0500

    Create headerEmpleado.php
    header empleado

---
+++ includes/headerEmpleado.php
@@ -0,0 +1,63 @@
+
+<div class="sidebar close">
+    <div class="logo-details">
+      <img src="img/logo.png" alt="" />
+      <span class="logo_name">Pastelería Dolce Rivoluzione</span>
+    </div>
+    <ul class="nav-links">
+      <li>
+        <a href="indexAdministrador.php">
+          <i class='fa-solid fa-house'></i>
+          <span class="link_name">Inicio</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a href="indexAdministrador.php" class="link_name">Inicio</a></li>
+        </ul>
+      </li>
+      <li>
+        <div class="icon-link">
+          <a href="pedidos.php">
+            <i class='fas fa-shopping-cart'></i>
+            <span class="link_name">Pedidos</span>
+          </a>
+        </div>
+        <ul class="sub-menu">
+          <li><a href="pedidos.php" class="link_name">Pedidos</a></li>
+
+        </ul>
+      </li>
+      <li>
+        <div class="icon-link">
+          <a href="listaproductos.php">
+            <i class='fas fa-box'></i>
+            <span class="link_name">Productos</span>
+          </a>
+        </div>
+        <ul class="sub-menu">
+          <li><a href="listaproductos.php" class="link_name">Productos</a></li>
+        </ul>
+      </li>
+      <li>
+        <a href="listarAdministrador.php">
+          <i class='fas fa-users'></i>
+          <span class="link_name">Usuarios</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a href="listarAdministrador.php" class="link_name">Usuarios</a></li>
+        </ul>
+      </li>
+
+      <li>
+        <div class="profile-details">
+          <div class="profile-content">
+            <i class="fa-solid fa-user"></i>
+          </div>
+          <div class="name-job">
+            <div class="profile_name">Usuario01</div>
+            <div class="job">Administrador</div>
+          </div>
+          <i class='fa-solid fa-arrow-right-from-bracket'></i>
+        </div>
+      </li>
+    </ul>
+  </div>
