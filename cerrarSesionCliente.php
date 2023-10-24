bbc57d8bece1ed3ec3775c6a96653123b77e1b01
maria <128330171+dokja777@users.noreply.github.com>
2023-10-23 22:21:00 -0500

    update cerrar session

--- cerrarSesionCliente.php
+++ cerrarSesionCliente.php
@@ -1,5 +1,11 @@
 <?php
 session_start();
-session_destroy();
-header("Location:inicioSesion.php");
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+  session_destroy();
+  header("Location: inicioSesion.php");
+} else {
+  // No se confirmó el cierre de sesión, puedes redirigir al usuario nuevamente a la página anterior o realizar alguna otra acción.
+  header("Location: pagina_anterior.php");
+}
 ?>
