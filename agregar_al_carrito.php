6a6db3f2e9119b756de7ea8ea01ca5aa5a32195d
maria <128330171+dokja777@users.noreply.github.com>
2023-10-24 22:05:19 -0500

    carrito

--- agregar_al_carrito.php
+++ agregar_al_carrito.php
@@ -107,6 +107,7 @@ $row = $resultado->fetch_assoc();
     integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
     crossorigin="anonymous" referrerpolicy="no-referrer" />
   <script src="js/jquery-3.7.1.min.js"></script>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
     crossorigin="anonymous"></script>
