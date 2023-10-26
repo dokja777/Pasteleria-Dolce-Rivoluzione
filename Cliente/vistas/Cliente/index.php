1d0a2cfe35008cef5054638ed255d4a077a08ba9
maria <128330171+dokja777@users.noreply.github.com>
2023-10-26 11:38:43 -0500

    nav bar y productos mas vendidos

---
+++ Cliente/vistas/Cliente/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="../../css/style.css" />
+	<link rel="stylesheet" href="../../css/styleIndexCliente.css" />
+	<script defer src="../../js/banner.js"></script>
+	<!-- Libreria splide -->
+	<link rel="stylesheet" href="../../recursos/libs/splide-4.1.3/splide-4.1.3/dist/css/splide.min.css" />
+	<script src="../../recursos/libs/splide-4.1.3/splide-4.1.3/dist/js/splide.min.js"></script>
+	<title>Inicio</title>
+</head>
+
+
+<body>
+	<!-- Configuración del navbar -->
+
+	<!-- Sliders -->
+
+	<section class="banner">
+		<div class="splide">
+			<div class="splide__track">
+				<ul class="splide__list">
+					<li class="splide__slide">
+						<img src="../../recursos/img/bannerPasteles.jpg" alt="" />
+					</li>
+					<li class="splide__slide">
+						<img src="../../recursos/img/bannerHalloween.jpg" alt="" />
+					</li>
+					<li class="splide__slide">
+						<img src="../../recursos/img/bannerPersonalizados.jpg" alt="" />
+					</li>
+				</ul>
+			</div>
+		</div>
+	</section>
+
+	<!-- Productos más vendidos -->
+
+	<h1 class="title">Productos Más Vendidos</h1>
+	<div class="container__masVendidos">
+		<?php include('../../../Servidor/PHP/Cliente/ProductosMasVendidos.php'); ?>
+		<?php foreach ($products as $product) { ?>
+		<div class="card">
+			<img src="data:image/jpg;base64, <?php echo base64_encode($product['IMG']); ?>" />
+			<h4>
+				<?php echo $product['N_PRODUCTO']; ?>
+			</h4>
+			<p><a>S/</a>
+				<?php echo $product['PRECIO']; ?>
+			</p>
+			<a href="DetalleProducto.php?id=<?php echo $product['ID_PRODUCTO']; ?>">
+				<button class="ver-detalle">Ver Detalle del Producto</button>
+			</a>
+		</div>
+		<?php } ?>
+	</div>
+
+
+
+	<!-- Footer -->
+
+
+</body>
+
+</html>
---
+++ Servidor/PHP/Cliente/ProductosMasVendidos.php
@@ -0,0 +1,24 @@
+<?php
+include(__DIR__ . '/../../conexion.php');
+$query = "SELECT
+        p.ID_PRODUCTO,
+        p.N_PRODUCTO,
+        p.IMG,
+        p.PRECIO
+        FROM producto p
+        JOIN (
+        SELECT dp.ID_PRODUCTO, SUM(dp.CANTIDAD) AS VENTAS
+        FROM detalle_pedido dp
+        JOIN pedido pe ON dp.ID_PEDIDO = pe.ID_PEDIDO
+        WHERE pe.ESTADO = 'Entregado'
+        GROUP BY dp.ID_PRODUCTO
+        ORDER BY VENTAS DESC
+        LIMIT 30 
+        ) top_products ON p.ID_PRODUCTO = top_products.ID_PRODUCTO;";
+$resultado = $conexion->query($query);
+$products = array();
+
+while ($row = $resultado->fetch_assoc()) {
+    $products[] = $row;
+}
+?>
