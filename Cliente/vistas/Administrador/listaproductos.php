0efde24db047094b641c4c5d2632c95ee5c20f50
maria <128330171+dokja777@users.noreply.github.com>
2023-11-03 16:24:33 -0500

    filtrado productos owo

--- Cliente/vistas/Administrador/listaproductos.php
+++ Cliente/vistas/Administrador/listaproductos.php
@@ -135,6 +135,8 @@
     AOS.init();
   </script>
 
+
+    <script src="../../../Cliente/js/filtradoBusquedaProductos.js"></script>
     <!-- Incluir Bootstrap JS y jQuery (opcional) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
