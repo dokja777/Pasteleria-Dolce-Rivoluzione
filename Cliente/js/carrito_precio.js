11c022002d3fb9cdcb2f4ddd271a2f9b7d83e68f
maria <128330171+dokja777@users.noreply.github.com>
2023-11-06 15:02:44 -0500

    modificar carrito

--- Cliente/js/carrito_precio.js
+++ Cliente/js/carrito_precio.js
@@ -1,27 +1,37 @@
-$(document).ready(function () {
-    $(".txtCantidad").keyup(function () {
-      var cantidad = $(this).val();
-      var precio = $(this).data('precio');
-      var id = $(this).data('id');
-      incrementar(cantidad, precio, id)
+document.addEventListener('DOMContentLoaded', function () {
+  // Este código se ejecutará cuando el DOM esté listo
+
+  // Función para actualizar el precio
+  function actualizarPrecio(input) {
+    var cantidad = $(input).val();
+    var precio = $(input).data('precio');
+    var id = $(input).data('id');
+    var mult = parseFloat(cantidad) * parseFloat(precio);
+    $(".cant" + id).text("S/ " + mult);
+    
+    $.ajax({
+      method: 'POST',
+      url: '../../../Servidor/PHP/Cliente/actualizarCarrito.php',
+      data: {
+        id: id,
+        cantidad: cantidad,
+      }
+    }).done(function (respuesta) {
+      // Puedes realizar acciones adicionales después de la actualización aquí
     });
-    $(".btnIncrementar").click(function () {
-      var cantidad = $(this).parent('div').parent('div').find('input').val();
-      var precio = $(this).parent('div').parent('div').find('input').data('precio');
-      var id = $(this).parent('div').parent('div').find('input').data('id');
-      incrementar(cantidad, precio, id)
-    });
-    function incrementar(cantidad, precio, id) {
-      var mult = parseFloat(cantidad) * parseFloat(precio);
-      $(".cant" + id).text("S/ " + mult);
-      $.ajax({
-        method: 'POST',
-        url: '../../../Servidor/PHP/Cliente/actualizarCarrito.php',
-        data: {
-          id: id,
-          cantidad: cantidad,
-        }
-      }).done(function (respuesta) {
-      });
+  }
+
+  // Manejo de eventos
+  $(".txtCantidad").on('input', function () {
+    actualizarPrecio(this);
+  });
+
+  $(".btnIncrementar").click(function () {
+    var cantidadInput = $(this).closest('tr').find('.txtCantidad');
+    var nuevaCantidad = parseInt(cantidadInput.val(), 10) + ($(this).hasClass('js-btn-plus') ? 1 : -1);
+    if (nuevaCantidad >= 1) {
+      cantidadInput.val(nuevaCantidad);
+      actualizarPrecio(cantidadInput);
     }
+  });
 });
--- Cliente/vistas/Cliente/agregar_al_carrito.php
+++ Cliente/vistas/Cliente/agregar_al_carrito.php
@@ -15,26 +15,27 @@ include('../../../Servidor/PHP/Cliente/carrito.php');
   <link rel="stylesheet" href="../../../Cliente/css/carrito.css" />
   <!-- Iconos en font awesome -->
   <link rel="stylesheet" href="../../../Cliente/css/style.css" />
+  <script src="../../../js/jquery-3.7.1.min.js"></script>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
     integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
     crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <script src="js/jquery-3.7.1.min.js"></script>
+
   <link rel="stylesheet" href="../../../Cliente/css/bootstrap.min.css">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
     crossorigin="anonymous"></script>
   <title>Carrito de Compra</title>
 
-  
+
 </head>
 
 <body>
 
   <!-- Configuración del navbar -->
 
-  <?php include('../../../Cliente/vistas/Cliente/headerCliente.php');?>
+  <?php include('../../../Cliente/vistas/Cliente/headerCliente.php'); ?>
 
   <!--Carrito de compras-->
 
@@ -58,7 +59,7 @@ include('../../../Servidor/PHP/Cliente/carrito.php');
                 </tr>
               </thead>
               <tbody>
-                <?php include('../../../Servidor/PHP/Cliente/funcionalidadCarrito.php');?>
+                <?php include('../../../Servidor/PHP/Cliente/funcionalidadCarrito.php'); ?>
               </tbody>
             </table>
             <div class=" col-6 " style="margin-left: 800px; border: 2px ;">
@@ -72,10 +73,9 @@ include('../../../Servidor/PHP/Cliente/carrito.php');
       </div>
 
       <!-- Footer -->
-      <iframe src="../../../Cliente/vistas/Cliente/footer.html" frameborder="0" scrolling="no" width="100%" height="320px"></iframe>
+      <iframe src="../../../Cliente/vistas/Cliente/footer.html" frameborder="0" scrolling="no" width="100%"
+        height="320px"></iframe>
 
-      <script src="../../../Cliente/js/carrito_precio.js"></script>
-
-      
 </body>
+
 </html>
