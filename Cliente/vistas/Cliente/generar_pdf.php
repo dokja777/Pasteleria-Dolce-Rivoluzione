eb5801f319918881561ac2f001bfbd1c368b5b47
z6nc <andresvelasquezarotinco@gmail.com>
2023-11-06 17:34:17 -0500

    the finished ballot

--- Cliente/vistas/Cliente/generar_pdf.php
+++ Cliente/vistas/Cliente/generar_pdf.php
@@ -2,24 +2,25 @@
 include('../../../Servidor/PHP/Cliente/sessionAbiertaCliente.php');
 require('../../../TCPDF-main/tcpdf.php');
 
-// Inicializar TCPDF
+// Inicializar TCPDF= es para el pdf ,esta descargado 
 $pdf = new TCPDF();
 $pdf->AddPage();
-$pdf->SetFont('helvetica', '', 12);
+$pdf->SetFont('courier', '', 11);
 
-// Crear el contenido del PDF
-$html = '<div style="background-color: #f8f8f8; padding: 20px; border: 1px solid #ccc; border-radius: 5px;box-shadow: 0 0 10px black; max-width: 400px; margin: 0 auto;" class="boleta">';
-$html .= '<div style=" display: flex; justify-content: space-between;  align-items: center;" class="header">';
-$html .= '<h1 style=" font-size: 24px;  margin-bottom: 10px;color: #783f04;"><strong>Boleta de Compra</strong></h1>';
-$html .= '<img class="imglogo" width="40px" src="../../../Cliente/recursos/img/logo.png" alt="" />';
+// Crear el contenido del PDF igual como esta en la archivo GenerarBoleta.php 
+$html = '<div style="background-color: #f8f8f8; padding: 20px; border: 1px solid #ccc; border-radius: 5px; box-shadow: 0 0 10px black; max-width: 400px; margin: 0 auto; text-align: center; " class="boleta">';
+$html .= '<div style="background-color: #f9cb9c;display: flex; justify-content: space-between; align-items: center;" class="header">';
+$html .= '<h1 style="font-size: 24px; margin-bottom: 10px; color: #783f04;"><strong>Patelera Dolce Rivoluzione</strong></h1>';
+$html .= '<h2 style="margin-bottom: 10px; color: #783f04;"><strong>Boleta de Compra</strong></h2>';
 $html .= '</div>';
 
-if(isset($_SESSION['carrito'])){
+//consular correspondiente para el carrito 
+if (isset($_SESSION['carrito'])) {
     $arregloCarrito = $_SESSION['carrito'];
     $totalCompra = 0;
 
-    foreach($arregloCarrito as $producto){
-        $html .= '<div class="producto">';
+    foreach ($arregloCarrito as $producto) {
+        $html .= '<div style="margin: 10px 0; padding-bottom: 10px; text-align: center;" class="producto">';
         $html .= '<p><strong>Nombre:</strong> ' . $producto['Nombre'] . '</p>';
         $html .= '<p><strong>Precio unitario:</strong> S/ ' . $producto['Precio'] . '</p>';
         $html .= '<p><strong>Cantidad:</strong> ' . $producto['Cantidad'] . '</p>';
@@ -31,13 +32,14 @@ if(isset($_SESSION['carrito'])){
     }
 
     // Información adicional
-    $html .= '<div class="info-adicional">';
-    $html .= '<p><strong>Número de pedido:</strong> ' . uniqid() . '</p>';
+    $html .= '<div class="info-adicional" style=" background-color: #f9cb9c;" >';
+    $html .= '<p padding-top: 10px; ><strong>Número de pedido:</strong> ' . uniqid() . '</p>';
     $html .= '<p><strong>Fecha de compra:</strong> ' . date('Y-m-d H:i:s') . '</p>';
+    $html .= '<p class="total" style="font-size:15px ;color: #783f04;"><strong>Total de la compra:</strong> S/ ' . $totalCompra . '</p>';
     $html .= '</div>';
 
-    // Total de la compra
-    $html .= '<p class="total"><strong>Total de la compra:</strong> S/ ' . $totalCompra . '</p>';
+   
+   
 }
 
 $html .= '</div>';
@@ -46,4 +48,4 @@ $html .= '</div>';
 $pdf->writeHTML($html, true, false, true, false, '');
 
 // Finalizar y descargar el PDF
-$pdf->Output('boleta_compra.pdf', 'D');
+$pdf->Output('GraciasPorLaCompra.pdf', 'D');
