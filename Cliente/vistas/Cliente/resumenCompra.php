57673d8588caadf1526837706709db43389212ad
Rcdy1 <132182943+Rcdy1@users.noreply.github.com>
2023-11-06 19:45:50 -0500

    Update resumenCompra.php

--- Cliente/vistas/Cliente/resumenCompra.php
+++ Cliente/vistas/Cliente/resumenCompra.php
@@ -69,7 +69,117 @@
                         <div class="tarjeta_debito">
                             <?php include ('../../../Cliente/vistas/Cliente/MetodosPago/tarjetacredito.php');?>
                             <a href="../../../Cliente/vistas/Cliente/GenerarBoleta.php"><button type="submit" >COMPRAR</button></a>
-                            
+                            <button id="btnFactura">COMPRAR CON FACTURA</button>
+
+                            <style>
+                                .modal {
+                                    display: none;
+                                    position: fixed;
+                                    z-index: 1;
+                                    left: 0;
+                                    top: 0;
+                                    width: 100%;
+                                    height: 100%;
+                                    overflow: auto;
+                                    background-color: rgba(0, 0, 0, 0.4);
+                                }
+
+                                .modal-contenido {
+                                    background-color: #fff;
+                                    margin: 10% auto;
+                                    padding: 20px;
+                                    border: 1px solid #888;
+                                    width: 40%;
+                                    border-radius: 10px;
+                                    
+                                }
+
+                                .cerrar {
+                                    color: #888;
+                                    float: right;
+                                    font-size: 28px;
+                                    font-weight: bold;
+                                    cursor: pointer;
+                                }
+                            </style>
+
+
+
+
+                            <!-- Modal de facturación -->
+                            <div id="modalFactura" class="modal">
+                                <div class="modal-contenido">
+                                    <span class="cerrar" id="cerrarModalFactura">&times;</span>
+                                    <h2>Ingrese los datos de facturación</h2>
+                                    <form action="GenerarFactura.php" method="post">
+                                        <br>
+                                       
+                                        <label for="ruc">RUC (Empresa):</label>
+                                        <input type="text" id="ruc" name="ruc" required>
+                                        <br><br>
+
+                                        <label for="nombre">Razon Social (Nombre):</label>
+                                        <input type="text" id="nombre" name="nombre" required>
+                                        <br><br>
+
+                                        <label for="direccion">Dirección:</label>
+                                        <input type="text" id="direccion" name="direccion" required>
+                                        <br><br>
+
+                                        <label for="departamento">Departamento:</label>
+                                        <input type="text" id="departamento" name="departamento" required>
+                                        <br><br>
+
+                                        <label for="provincia">Provincia:</label>
+                                        <input type="text" id="provincia" name="provincia" required>
+                                        <br><br>
+
+                                        <label for="distrito">Distrito:</label>
+                                        <input type="text" id="distrito" name="distrito" required>
+                                        <br><br>
+
+                                        <label for="codigo_postal">Código Postal:</label>
+                                        <input type="text" id="codigo_postal" name="codigo_postal">
+                                        <br><br>
+
+                                        <label for="telefono">Teléfono de contacto:</label>
+                                        <input type="tel" id="telefono" name="telefono">
+                                        <br><br>
+
+                                        <label for="email">Correo Electrónico:</label>
+                                        <input type="email" id="email" name="email" required>
+                                        <br><br>
+
+                                        <br>
+                                        <button type="submit">Generar Factura</button>
+                                        <button id="btnFactura">Generar Factura1</button>
+                                    </form>
+                                </div>
+                            </div>
+
+                            <script>
+                                
+                                var btnFactura = document.getElementById('btnFactura');
+                                var modalFactura = document.getElementById('modalFactura');
+                                var cerrarModalFactura = document.getElementById('cerrarModalFactura');
+
+                                // abrir el modal
+                                function abrirModal() {
+                                    modalFactura.style.display = 'block';
+                                }
+
+                                //  cerrar el modal
+                                function cerrarModal() {
+                                    modalFactura.style.display = 'none';
+                                }
+
+                                // abrir el modal al hacer clic en el botón
+                                btnFactura.addEventListener('click', abrirModal);
+
+                                //  cerrar el modal al hacer clic en la "X"
+                                cerrarModalFactura.addEventListener('click', cerrarModal);
+                            </script>
+
                         </div>
                     </div>
                     <div id="pago2" class="pago">Contenido de la Opción 2</div>
