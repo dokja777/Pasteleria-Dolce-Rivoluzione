647dc2293baa6ed3db5499497b325a84a62c1882
Alexandra-VP <146406971+Alexandra-VP@users.noreply.github.com>
2023-11-12 00:35:46 -0500

    Update eliminarPedido.php

--- Servidor/PHP/EmpleadoServidor/eliminarPedido.php
+++ Servidor/PHP/EmpleadoServidor/eliminarPedido.php
@@ -2,14 +2,14 @@
     //conexion a la base de datos
     include('../../../Servidor/conexion.php');
 
-    $idEmpleado = $_GET['ID_PEDIDO'];
+    $idpedido = $_GET['ID_PEDIDO'];
     $sql = "DELETE FROM pedido where ID_PEDIDO = '$idpedido'";
 
     $query = mysqli_query($conexion, $sql);
 
     if ($query) {
         // Empleado eliminado exitosamente
-        echo "<script>alert('Empleado eliminado exitosamente'); window.location.href='../../../Cliente/vistas/Empleado/pedidos.php';</script>";
+        echo "<script>alert('Pedido eliminado exitosamente'); window.location.href='../../../Cliente/vistas/Empleado/pedidos.php';</script>";
     } else {
         // No se pudo eliminar
         echo "<script>alert('No se pudo eliminar'); window.history.go(-1);</script>";
