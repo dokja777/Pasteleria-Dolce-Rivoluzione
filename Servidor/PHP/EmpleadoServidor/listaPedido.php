90434f77e271fa45c7803e32158e72b142869187
Alexandra-VP <146406971+Alexandra-VP@users.noreply.github.com>
2023-11-11 23:57:35 -0500

    eliminar pedido

--- Servidor/PHP/EmpleadoServidor/listaPedido.php
+++ Servidor/PHP/EmpleadoServidor/listaPedido.php
@@ -44,7 +44,7 @@
               <a href='../Empleado/editarPedidoEm.php?id=$idpedido' class=\"btn btn-warning\"><i class='fas fa-pencil-alt'></i></a>
               <br>
               <br>
-              <a href='../../../Servidor/PHP/EmpleadoServidor/eliminarPedido.php?ID_PEDIDO=$idpedido'class=\"btn btn-danger\"><i class='fas fa-trash-alt'></i>
+              <a href='../../../Servidor/PHP/EmpleadoServidor/eliminarPedido.php?ID_PEDIDO=$idpedido'class=\"btn btn-danger eliminar_pedido\"><i class='fas fa-trash-alt'></i>
               </a>
               </td>";
             echo "<td>
