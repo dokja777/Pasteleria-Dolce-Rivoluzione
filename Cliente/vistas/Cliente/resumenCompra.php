ec3dba942b9082f6331942af2085c2817ad840db
maria <128330171+dokja777@users.noreply.github.com>
2023-12-06 11:52:44 -0500

    Update resumenCompra.php

--- Cliente/vistas/Cliente/resumenCompra.php
+++ Cliente/vistas/Cliente/resumenCompra.php
@@ -59,11 +59,6 @@
                         <input type="radio" id="yape" name="opciones" value="yape">
                         <label for="yape">Yape</label>
 
-                        <input type="radio" id="paypal" name="opciones" value="paypal">
-                        <label for="paypal">Paypal</label>
-
-                        <input type="radio" id="efectivo" name="opciones" value="efectivo">
-                        <label for="efectivo">Efectivo</label>
                     </form>
 
                     <div id="pago1" class="pago">
@@ -241,11 +236,7 @@
                                                 return 'Débito';
                                             } else if (valorSeleccionado === 'yape') {
                                                 return 'Yape';
-                                            } else if (valorSeleccionado === 'paypal') {
-                                                return 'Paypal';
-                                            } else if (valorSeleccionado === 'efectivo') {
-                                                return 'Efectivo';
-                                            }
+                                            } 
                                         }
                                     }
 
@@ -275,6 +266,187 @@
                                 <input type="checkbox" id="aceptoTerminos" name="aceptoTerminos">
                                 <label for="aceptoTerminos">Acepto los términos y condiciones</label>
                             </p>
+                            <a href="../../../Cliente/vistas/Cliente/GenerarBoleta.php" onclick="return confirmarCompra('boleta')">
+                                <button type="submit">COMPRAR CON BOLETA</button>
+                            </a>
+                            <button type="submit" id="btnFactura" >COMPRAR CON FACTURA</button>
+
+                            <style>
+                                .modal {
+                                    display: none;
+                                    position: fixed;
+                                    z-index: 1;
+                                    left: 0;
+                                    top: 0;
+                                    width: 100%;
+                                    height: 100%;
+                                    overflow: auto;
+                                    background-color: rgba(0, 0, 0, 0.4);
+                                }
+
+                                .modal-contenido {
+                                    background-color: #fff;
+                                    margin: 10% auto;
+                                    padding: 20px;
+                                    border: 1px solid #888;
+                                    width: 40%;
+                                    border-radius: 10px;
+                                    
+                                }
+
+                                .cerrar {
+                                    color: #888;
+                                    float: right;
+                                    font-size: 28px;
+                                    font-weight: bold;
+                                    cursor: pointer;
+                                }
+                            </style>
+
+
+
+
+                            <!-- Modal de facturación -->
+                            <div id="modalFactura" class="modal">
+                                <div class="modal-contenido">
+                                    <span class="cerrar" id="cerrarModalFactura">&times;</span>
+                                    <h2>Ingrese los datos de facturación</h2>
+                                    <form action="GenerarFactura.php" method="post" onsubmit="return confirmarCompra('factura')">
+                                        <br>
+                                       
+                                        <label for="ruc">RUC (Empresa):</label>
+                                        <input type="text" id="ruc" name="ruc" required>
+                                        <br><br>
+
+                                        <label for="nombre">Razon Social (Nombre):</label>
+                                        <input type="text" id="nombre" name="nombre" required>
+                                        <br><br>
+
+                                        <label for="direccion">Dirección:</label>
+                                        <input type="text" id="direccion" name="direccion" required>
+                                        <br><br>
+
+                                        <label for="departamento">Departamento:</label>
+                                        <input type="text" id="departamento" name="departamento" required>
+                                        <br><br>
+
+                                        <label for="provincia">Provincia:</label>
+                                        <input type="text" id="provincia" name="provincia" required>
+                                        <br><br>
+
+                                        <label for="distrito">Distrito:</label>
+                                        <input type="text" id="distrito" name="distrito" required>
+                                        <br><br>
+
+                                        <label for="codigo_postal">Código Postal:</label>
+                                        <input type="text" id="codigo_postal" name="codigo_postal">
+                                        <br><br>
+
+                                        <label for="telefono">Teléfono de contacto:</label>
+                                        <input type="tel" id="telefono" name="telefono"  class="pago-section-input">
+                                        <br><br>
+
+                                        <label for="email">Correo Electrónico:</label>
+                                        <input type="email" id="email" name="email" class="pago-section-input" required>
+                                        <br><br>
+
+                                        <br>
+                                        
+                                        <button id="btnFactura" class="btn-compra" >Generar Factura</button>
+
+                                    </form>
+                                </div>
+                            </div>
+
+                            <script>
+                                
+                                var btnFactura = document.getElementById('btnFactura');
+                                var modalFactura = document.getElementById('modalFactura');
+                                var cerrarModalFactura = document.getElementById('cerrarModalFactura');
+
+                                // abrir el modal
+                                function abrirModal() {
+                                    modalFactura.style.display = 'block';
+                                }
+
+                                //  cerrar el modal
+                                function cerrarModal() {
+                                    modalFactura.style.display = 'none';
+                                }
+
+                                // abrir el modal al hacer clic en el botón
+                                btnFactura.addEventListener('click', abrirModal);
+
+                                //  cerrar el modal al hacer clic en la "X"
+                                cerrarModalFactura.addEventListener('click', cerrarModal);
+                            
+                                function confirmarCompra(tipoCompra) {
+                                    var mensaje = "¿Estás seguro de realizar la compra con " + tipoCompra + "?";
+                                    var confirmacion = confirm(mensaje);
+
+                                    if (!confirmacion) {
+                                        // Si el usuario cancela la confirmación, evita que el formulario se envíe
+                                        return false;
+                                    } else {
+                                        // Obtiene el método de pago seleccionado
+                                        var metodoPago = obtenerMetodoPagoSeleccionado();
+
+                                        // Si no se seleccionó ningún método de pago, muestra un mensaje y evita el envío del formulario
+                                        if (!metodoPago) {
+                                            alert("Por favor, selecciona un método de pago.");
+                                            return false;
+                                        }
+
+                                        // Si la confirmación es verdadera, realiza una petición AJAX para enviar la fecha de recojo y el método de pago
+                                        var fechaRecojo = document.getElementById('fecha_recojo').value;
+
+                                        var xhr = new XMLHttpRequest();
+                                        xhr.open('POST', '../../../Servidor/PHP/Cliente/RegistrarPedido.php', true);
+                                        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+                                        xhr.onreadystatechange = function () {
+                                            if (xhr.readyState === 4) {
+                                                if (xhr.status === 200) {
+                                                    // Maneja la respuesta del servidor
+                                                    console.log(xhr.responseText);
+                                                } else {
+                                                    // Maneja errores de la solicitud AJAX
+                                                    console.error('Error en la solicitud AJAX:', xhr.status, xhr.statusText);
+                                                }
+                                            }
+                                        };
+
+                                        // Construye los datos a enviar
+                                        var datos = "fecha_recojo=" + encodeURIComponent(fechaRecojo) + "&metodo_pago=" + encodeURIComponent(metodoPago);
+
+                                        // Envía la petición
+                                        xhr.send(datos);
+
+                                        // Evita el envío del formulario si estás manejando todo en el mismo bloque de código
+                                        return true;
+                                    }
+                                }
+
+                                function obtenerMetodoPagoSeleccionado() {
+                                    var opcionesMetodoPago = document.getElementsByName('opciones');
+
+                                    for (var i = 0; i < opcionesMetodoPago.length; i++) {
+                                        if (opcionesMetodoPago[i].checked) {
+                                            var valorSeleccionado = opcionesMetodoPago[i].value;
+
+                                            // Identifica el método de pago según el valor seleccionado
+                                            if (valorSeleccionado === 'tarjeta_debito') {
+                                                return 'Débito';
+                                            } else if (valorSeleccionado === 'yape') {
+                                                return 'Yape';
+                                            } 
+                                        }
+                                    }
+
+                                    // Retorna null si no se seleccionó ningún método de pago
+                                    return null;
+                                }
+                            
+                            </script>
                         </div>
                     </div>
 
